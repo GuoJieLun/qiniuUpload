bfd18aa23b0f25014f6da7b9d10d800dd700a0d6
jay <522425760@qq.com>
2018-02-11 16:59:53 +0800

    updated

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # qiniuUpload
 node+qiniu 上传图片
+
+### 启动demo ###
+`node index`
+打开 `localhost:8234` 
--- upload.js
+++ upload.js
@@ -2,10 +2,38 @@ const qiniu = require("qiniu");
 
 const accessKey = '你的accessKey';
 const secretKey = '你的secretKey';
+
+/**
+ * 鉴权对象mac
+ * @type {auth.digest.Mac|*|Mac}
+ */
 const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+
+/**
+ * 构建配置类
+ * @type {conf.Config|exports.Config}
+ */
 const config = new qiniu.conf.Config();
+
+/**
+ * 初始化上传对象
+ * @type {form_up.FormUploader|*|FormUploader}
+ */
 const formUploader = new qiniu.form_up.FormUploader(config);
+
+
+/**
+ * 扩展参数
+ * @type {resume_up.PutExtra|form_up.PutExtra|*|PutExtra}
+ */
 const putExtra = new qiniu.form_up.PutExtra();
+
+
+/**
+ * 资源管理相关的操作首先要构建对象
+ * （bucketManager 应该是获取操作权限的） PS：纯属个人克烈
+ * @type {rs.BucketManager|*|BucketManager}
+ */
 const bucketManager = new qiniu.rs.BucketManager(mac, config);
 
 const path = require('path');
@@ -30,6 +58,12 @@ export default class UploadFile {
 
     }
 
+    /**
+     * 获取到验证的token
+     * @param bucket
+     * @param key
+     * @returns {string}
+     */
     static uptoken(bucket, key) {
         let options = {
             scope: bucket + ":" + key
@@ -39,6 +73,13 @@ export default class UploadFile {
         return uploadToken;
     }
 
+    /**
+     * 上传
+     * @param uptoken
+     * @param key
+     * @param localFile
+     * @returns {Promise}
+     */
     static  uploadFile(uptoken, key, localFile) {
         return new Promise((resolve, reject) => {
             formUploader.putFile(uptoken, key, localFile, putExtra, function (respErr, respBody, respInfo) {
@@ -69,6 +110,11 @@ export default class UploadFile {
         });
     }
 
+    /**
+     * 删除
+     * @param key 要删除的图片名
+     * bucket 要删除的图片所属空间
+     */
     static delImg(key) {
         let bucket = "test";
         var key = "my-nodejs-logo.png";
